6fe3fcd6d1c025ea7140b39082a84c6fe89ae8c0
Joachim Andersson <joaan879@student.liu.se>
2025-04-14 11:17:17 +0200

    change

---
+++ Project 2/Project_2.m
@@ -0,0 +1,5 @@
+
+mean = readmatrix('portfolioManagerV4Project 2 2025.xls', 'Sheet', 'assetStat', 'Range', 'B3:B12');
+covMatrix = readmatrix('portfolioManagerV4Project 2 2025.xls', 'Sheet', 'assetStat', 'Range', 'Q3:Z12');
+cont_estr = readmatrix('portfolioManagerV4Project 2 2025.xlsm', 'Sheet', 'Refinitiv', 'Range', 'N4:N4');
+
