bfef9096c9b83744f84afc131d44675de603f98f
William Eriksson <wiler441@student.liu.se>
2025-04-23 21:12:55 +0200

    Progress on project 3

--- Project 3/Options.m
+++ Project 3/Options.m
@@ -5,7 +5,8 @@ q = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range',
 r = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'W4:W9');
 K = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'X4:X9');
 Maturity = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'W1:W1');
-
+BS_Call = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'Y4:Y9');
+BS_Put = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'Z4:Z9');
 
 red_days = [
     '2025-04-17'; % Maundy Thursday (Skærtorsdag)
@@ -20,20 +21,34 @@ Maturity_date = datetime(Maturity, 'ConvertFrom', 'excel');
 % Initialize T vector
 T = zeros(size(t));
 IV = zeros(size(t));
+vega = zeros(size(t));
+gamma = zeros(size(t));
+delta_call = zeros(size(t));
+delta_put = zeros(size(t));
 
 % Calculate business days for each date in t
 for i = 1:length(t)
     T(i) = days252bus(t_dates(i), Maturity_date, red_days)/252;
-    %VAD SKA OPTIONS VÄRDET VARA????????????????????????
-   % IV(i)= Implicit_sigma(10,S(i),K(i),r(i),q(i),T(i),0.00001, 0.2);
+    IV(i)= Implicit_sigma(BS_Call(i),S(i),K(i),r(i),q(i),T(i),0.00001, 0.2);
+    [delta, vega_, gamma_] = CalculateGreeks(S(i), K(i), r(i), q(i), T(i), IV(i), true);
+    delta_call(i) = delta;
+    vega(i) = vega_;
+    gamma(i) = gamma_;
+
+    [delta, vega_, gamma_] = CalculateGreeks(S(i), K(i), r(i), q(i), T(i), IV(i), false);
+    delta_put(i) = delta;
 end
 
+    %writematrix(IV,'portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Answer', 'Range', 'L8:L13')
+    %writematrix(delta_call,'portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Answer', 'Range', 'M8:M13')
+    %writematrix(delta_put,'portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Answer', 'Range', 'N8:N13')
+    %writematrix(gamma,'portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Answer', 'Range', 'O8:O13')
+    %writematrix(vega,'portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Answer', 'Range', 'P8:P13')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Task 5 in step by step
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-window_size = 500;          % Historical window for simulations
 noOfSimulations = 500;      % Number of scenarios to generate
 confidence_level = 0.95;    % For 95% VaR
 risk_limit = 1e6;           % 1 million DKK VaR limit
@@ -42,7 +57,8 @@ portfolio_values = zeros(noOfSimulations, 1);
 option_values_call = zeros(noOfSimulations, 1);
 option_values_put = zeros(noOfSimulations, 1);
 
-index_prices = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'AB5:AB758');
+
+index_prices = readmatrix('portfolioManagerV4Projekt3.xlsm', 'Sheet', 'Refinitiv', 'Range', 'AF6:AF2086');
 
 log_returns = diff(log(index_prices));
 VaR_95 = zeros(length(t),1);
@@ -57,11 +73,8 @@ for day = 1:length(t)
     div_yield = q(day);
     time_to_maturity = T(day);
 
-    start_idx = max(1, day-window_size);
-    end_idx = day-1;
-    available_returns = log_returns(start_idx:end_idx);
     % Each day, you simulate 500 scenarios of the index (based on past returns and current index.
-    random_indices = randi(length(log_returns), noOfSimulations, 1);
+    random_indices = randi(numel(log_returns), noOfSimulations, 1);
     simulated_log_returns = log_returns(random_indices);
  
     % Convert to prices
@@ -84,6 +97,7 @@ for day = 1:length(t)
     [current_put_delta, ~, ~] = CalculateGreeks(current_price, strike, rate, div_yield, time_to_maturity, IV(day), false);
 
     % Start with equal quantities (will be scaled)
+    
     nCall = 100;
     nPut = round(-nCall * current_call_delta / current_put_delta);
 
@@ -104,19 +118,19 @@ for day = 1:length(t)
     alpha = 1 - confidence_level;
     VaR_95(day) = -prctile(sorted_losses , alpha * 100);
     
-    if day == 1 
+ 
      if VaR_95 > risk_limit
         scale_factor = risk_limit / VaR_95;
         nCall = floor(nCall * scale_factor);
         nPut = floor(nPut * scale_factor);
         fprintf('Positions scaled to %d calls and %d puts\n', nCall, nPut);
      end
-    end
+
 
     portfolio_history(day).date = t_dates(day);
     portfolio_history(day).nCall = nCall;
     portfolio_history(day).nPut = nPut;
-    portfolio_history(day).VaR = VaR_95;
+    portfolio_history(day).VaR = VaR_95(day);
     portfolio_history(day).delta = nCall * current_call_delta + nPut * current_put_delta; 
 
 end
